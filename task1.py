1e0e8e1ff1f4baac95b1456690e045f98dac9ed8
Ruslan <6r0d5k1y@gmail.com>
2022-10-12 23:52:58 +0300

    commit

--- task1.py
+++ task1.py
@@ -16,7 +16,7 @@ for slog in range(1, count_slog + 1):
         list2 = []# список игроков
         list2.append(i)
 
-        print(list2, "list 2")
+        print(list2)
         print(count_slog)
     for i in range(0, count_pleyer):
         if count_slog%slog == 0:
