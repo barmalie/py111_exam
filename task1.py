98c7f59071ea6e16f9d65ba8052b8f4d97ad55c2
Ruslan <6r0d5k1y@gmail.com>
2022-10-12 23:29:02 +0300

    какая то дичь

--- task1.py
+++ task1.py
@@ -2,25 +2,24 @@ text = "flfkxkckg lglglglvl glofkg, fkfkfkf."
 modify_text = text.replace(" ", "")
 modify_text = modify_text.replace(",", "")
 modify_text = modify_text.replace(".", "")
-# print(modify_text)
+
 
 count_slog = len(modify_text) // 2
 for slog in range(1, count_slog + 1):
-    list = []
+    list = []# список слогов
     list.append(slog)
-    print(slog)
+
 
     count_pleyer = 3
     assert count_pleyer < count_slog
-    # for i in range(1, count_pleyer+1):
-    # list2 = []
-    # list2.append(i)
-    # print(i)
-    # if count_slog == slog:
-    # while list2 > list2[0]:
-    # print(i,slog,"game over")
-    # list2.pop()
-    # print(i,slog,"game over")
-    print([i for i in iter(iter(range(count_pleyer)).__next__, count_pleyer)])
-    if count_slog == slog:
-        print(i, slog, "game over")
+    for i in range(1, count_pleyer+1):
+        list2 = []# список игроков
+        list2.append(i)
+
+        print(list2, "list 2")
+        print(count_slog)
+    for i in range(0, count_pleyer):
+        if count_slog%slog == 0:
+            list2.pop(slog) #out of range
+            print(list2)
+            print(slog)
