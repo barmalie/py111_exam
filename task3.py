f87ec9480f3f167e022ce4e0aea4e3cb06226aaa
Ruslan <6r0d5k1y@gmail.com>
2022-10-12 22:46:53 +0300

    какая то дичь

--- task3.py
+++ task3.py
@@ -1,35 +1,50 @@
 import random
 
-def repeat(number=1):
-    """ Повтор декорированной функции заданное количество раз.
-    В результате возвращается последнее значение оригинальной функции.
-    : number — количество повторов, по умолчанию равно 3.
-    """
-
-    def actual_decorator(function):
-        def wrapper(*args, **kwargs):
-            result = None
-            for _ in range(number):
-                result = function(*args, **kwargs)
-            return result
-
-        return wrapper
-
-    return actual_decorator
-
-
-@repeat(20)
+# def repeat(number=1):
+#     """ Повтор декорированной функции заданное количество раз.
+#     В результате возвращается последнее значение оригинальной функции.
+#     : number — количество повторов, по умолчанию равно 3.
+#     """
+#
+#     def actual_decorator(function):
+#         def wrapper(*args, **kwargs):
+#             result = None
+#             for _ in range(number):
+#                 result = function(*args, **kwargs)
+#             return result
+#
+#         return wrapper
+#
+#     return actual_decorator
+#
+#
+# @repeat(20)
 def print_my_call():
     count = []
-    start_time = random.randint(0, 23)
-    end_time = random.randint(1, 24)
-    if start_time > end_time:
-        zayavka = (end_time, start_time)
-        count.append(zayavka)
-        print(count, end=' ')
-        print(len(count))
+    range_ = range(random.randint(1, 23))
+    for i in range_:
+        start_time = random.randint(0, 23)
+        end_time = random.randint(1, 24)
+        zayavka = (start_time, end_time)
 
+        if start_time < end_time:
+            count.append(zayavka)
 
+    sort_index = sorted(count, key=lambda zayavka :zayavka[0])
+    print(sort_index)
+    n = 0
+
+    list_wish = []
+    for wish in sort_index:
+        try:
+            sort_index[n+1][0] > sort_index[n][1]
+            list_wish.append(wish)
+
+            print(list_wish)
+        except:
+            print(wish)
+
+    #print(list_wish[n])
 
 
 print_my_call()
