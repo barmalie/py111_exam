fef88f3c45f9720be0fa7b856b5cd0b776f86eff
Ruslan <6r0d5k1y@gmail.com>
2022-10-16 23:27:43 +0300

    test

--- task1.py
+++ task1.py
@@ -23,3 +23,20 @@ for slog in range(1, count_slog + 1):
             list2.pop(slog) #out of range
             print(list2)
             print(slog)
+
+# Считалка
+a = list(map(int, input().split()))
+list_count_player = [i for i in range(1, a[0] + 1)]  # формируем список из кол-во человек
+list_player_exit = []  # новый массив для списка из тех кто выбыл
+i1 = 1
+
+while 0 < len(list_count_player):  # пока в массиве не останется 1 элемент
+    while i1 <= a[1]:  # цикл накручивает до второго числа ( который мы вводили в инпут )
+        if i1 == a[1]:  # тут если он проходит проверку идет дальше
+            for i1 in list_count_player:  # начинаем считать с этого индекса
+                list_player_exit.append(list_count_player[i1])  # и сразу же добавляем в новый
+                list_count_player.remove(i1)  # а отсюда удаляем
+                i1 = 0  # ставим счетчик на ноль чтобы след действие не сбило его
+        i1 += 1  # и увеличиваем до первоначального состояния
+
+print(list_player_exit)  # выводим новый массив
