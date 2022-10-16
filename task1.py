59135f5bbfdefa3f51d6c0a61ddb6da0a5ff98c3
Ruslan <6r0d5k1y@gmail.com>
2022-10-16 23:35:41 +0300

    test

--- task1.py
+++ task1.py
@@ -1,38 +1,38 @@
 text = "flfkxkckg lglglglvl glofkg, fkfkfkf."
-modify_text = text.replace(" ", "")
-modify_text = modify_text.replace(",", "")
-modify_text = modify_text.replace(".", "")
-
-
-count_slog = len(modify_text) // 2
-for slog in range(1, count_slog + 1):
-    list = []# список слогов
-    list.append(slog)
-
-
-    count_pleyer = 3
-    assert count_pleyer < count_slog
-    for i in range(1, count_pleyer+1):
-        list2 = []# список игроков
-        list2.append(i)
-
-        print(list2)
-        print(count_slog)
-    for i in range(0, count_pleyer):
-        if count_slog%slog == 0:
-            list2.pop(slog) #out of range
-            print(list2)
-            print(slog)
+# modify_text = text.replace(" ", "")
+# modify_text = modify_text.replace(",", "")
+# modify_text = modify_text.replace(".", "")
+#
+#
+# count_slog = len(modify_text) // 2
+# for slog in range(1, count_slog + 1):
+#     list = []# список слогов
+#     list.append(slog)
+#
+#
+#     count_pleyer = 3
+#     assert count_pleyer < count_slog
+#     for i in range(1, count_pleyer+1):
+#         list2 = []# список игроков
+#         list2.append(i)
+#
+#         print(list2)
+#         print(count_slog)
+#     for i in range(0, count_pleyer):
+#         if count_slog%slog == 0:
+#             list2.pop(slog) #out of range
+#             print(list2)
+#             print(slog)
 
 # Считалка
-a = list(map(int, input().split()))
+a= [10]
+#a = list(map(int, input().split()))
 list_count_player = [i for i in range(1, a[0] + 1)]  # формируем список из кол-во человек
 list_player_exit = []  # новый массив для списка из тех кто выбыл
 i1 = 1
-
 while 0 < len(list_count_player):  # пока в массиве не останется 1 элемент
-    while i1 <= a[1]:  # цикл накручивает до второго числа ( который мы вводили в инпут )
-        if i1 == a[1]:  # тут если он проходит проверку идет дальше
+    while i1 <= a[0]:  # цикл накручивает до второго числа ( который мы вводили в инпут )
+        if i1 == a[0]:  # тут если он проходит проверку идет дальше
             for i1 in list_count_player:  # начинаем считать с этого индекса
                 list_player_exit.append(list_count_player[i1])  # и сразу же добавляем в новый
                 list_count_player.remove(i1)  # а отсюда удаляем
