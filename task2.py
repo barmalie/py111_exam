5aa2cd1d4ed578c6c924a4295961e2119d254b70
Ruslan <6r0d5k1y@gmail.com>
2022-10-22 09:51:22 +0300

    commit

--- task2.py
+++ task2.py
@@ -24,19 +24,19 @@ for i in range(1, len(coasts[2])):
     total_coasts[i][2] = total_coasts[i-1][2] + coasts[i][2]
 print(coasts)
 print(total_coasts)
-
+def calculate_path(total_costs, point):
 rows = total_coasts[1][0]
 cols = total_coasts[0][1]
-def get_step(i,j):
-    if i == 0 and j == 0:
-        return 1
-    if not 0<=i<= rows:
-        return 0
-    if not 0<=j<= cols:
-        return 0
-    return sum([get_step(i + 1,j + 1),])
-                get_step(i - 1, j + 1),
-                get_step(i + 1, j - 1),
-                get_step(i - 1, j - 1),
-                get_step(i , j )])
-    return get_step(i,j)
+    def get_step(i, j):
+        if i == 0 and j == 0:
+            return 1
+        if not 0<=i<= rows:
+            return 0
+        if not 0<=j<= cols:
+            return 0
+        return sum([get_step(i + 1,j + 1),])
+                    get_step(i - 1, j + 1),
+                    get_step(i + 1, j - 1),
+                    get_step(i - 1, j - 1),
+                    get_step(i , j )])
+        return get_step(i = point[0], j = point[1)
